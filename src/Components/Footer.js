4cfb761a13a64f3b1ccff3e704378008f0a09a69
Blessing Jemil <136798837+BlessingJemil@users.noreply.github.com>
2023-10-28 03:54:36 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,23 +4,7 @@ const Footer = () => {
   return (
     <>
         <footer>
-            <div className='container text-center top'>
-                <div className='navlink'>
-                        <ul className='d_flex'>
-                        <li><a href="#home">home</a></li>
-                        <li><a href="#features">Skills</a></li>
-                        <li><a href="#portfolio">projects</a></li>
-                       <li><a href="#education">education</a></li>
-                        <li><a href="#contact">contact</a></li>
-                        <li></li>
-                        
-                        
-                        
-                        
-                    </ul>
-                    <br></br>
-                    <br></br>
-                </div>
+          
                 <p>© 2023. All rights reserved by Blessing Jemil - Developer</p>
             </div>
         </footer>
